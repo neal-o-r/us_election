86a4c8aab0b0bd8de3dd2bf03341295cfe821fcc
neal-o-r <neal@n-o-r.xyz>
2016-11-22 17:09:48 +0000

    trellis plot

--- analysis.py
+++ analysis.py
@@ -1,39 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import load_data as ld
+import seaborn as sns; sns.set()
 
-plt.style.use('fivethirtyeight')   
 
 with open("cnn_data.json") as f:
         df = ld.cnn_dataframe(f)
 
 print("There were %d questions asked"%len(set(df.question)))
 
-def add_color(name):
-        dem_blue = "#3333FF"
-        rep_red  = "#EE3523"
-        neutral  = "#DDDDDD"
-
-        if name == 'Hillary':
-                return dem_blue
-        elif name == 'Donald':
-                return rep_red
-        else:
-                return neutral
-
 def ask_a_question(df, que):
 
+	c_pal = {'Hillary':'#3333FF', 'Donald':'#EE3523', 'N/A':'#DDDDDD'}
+
         df_q = df[df.question == que]
-        df_q = df_q.set_index('answer')
 
-        df_q.candidate_pct.plot(kind='bar', color=df.Colour)
+	g = sns.FacetGrid(df_q, col='answer', hue='candidate', col_wrap=3)
+        g.map(sns.barplot, 'candidate', 'candidate_pct', palette=c_pal)
+	g.set_xticklabels(rotation=90)
+	g.set_ylabels('Candidate %')
+	plt.ylim(0,100)
+	plt.show()
 
-        plt.title(que)
-        plt.ylabel('Candidate %')
-        plt.show()
 
-
-
-df['Colour'] = df.candidate.apply(add_color)
 
 ask_a_question(df, 'area type')
+
